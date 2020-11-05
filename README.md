121dcfa2a4321dddd8bce1bc33c48cff1bc887ac
Israel Hudson <israelhudson@gmail.com>
2020-11-05 09:11:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_grocery_list
+ Prjeto prático de uma lista de compras para ajudar na ida ao supermercado.
---
+++ android/app/src/main/kotlin/br/com/academy/flutter_grocery_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.academy.flutter_grocery_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
